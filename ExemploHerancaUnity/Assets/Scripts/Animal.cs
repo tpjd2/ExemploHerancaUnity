8e3b8a6c6300615efc487136f904d198a8effd7c
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-09-17 16:21:22 -0300

    inicio do projeto: desenvolvimento da classe pai

---
+++ ExemploHerancaUnity/Assets/Scripts/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] string nome;
+    [SerializeField] float velocidadeVertical;
+
+    Vector3 posicaoInicial;
+    Vector3 posicao;
+
+    private void Start()
+    {
+        posicaoInicial = transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    protected virtual void Move()
+    {
+        posicao = posicaoInicial + Vector3.right * Mathf.Sin(Time.time);
+        velocidadeVertical -= 14 * Time.deltaTime;
+        
+        if (velocidadeVertical < 0)
+            velocidadeVertical = 0;
+
+        posicao.y = velocidadeVertical + posicao.y;
+        
+        transform.position = posicao;
+    }
+
+    protected virtual void Fala()
+    {
+        Debug.Log("O animal diz: Bem-vindo");
+    }
+
+    protected virtual void Pula()
+    {
+        velocidadeVertical = 2.0f;
+    }
+}
